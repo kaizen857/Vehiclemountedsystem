e5cbfd2ace5c82b3d692f4992dc69b4d05f0558a
kaizen857 <3064561709@qq.com>
2026-06-10 16:19:52 +0800

    media listener
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ app/src/main/java/com/example/vehicle_mountedsystem/data/media/MediaNotificationListenerService.java
@@ -0,0 +1,6 @@
+package com.example.vehicle_mountedsystem.data.media;
+
+import android.service.notification.NotificationListenerService;
+
+public final class MediaNotificationListenerService extends NotificationListenerService {
+}
